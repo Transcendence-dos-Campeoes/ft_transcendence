3fa0f22203381238accbe886b93e7211fd9c9d50
Jorge Costa <Kelho88@pm.me>
2025-01-13 15:33:15 +0000

    fix: correct spelling of 'transcendence' in script, README, and docker-compose files

--- Backend/script.sh
+++ Backend/script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# filepath: /home/masoares/ft_transcendance/Backend/start.sh
+# filepath: /home/masoares/ft_transcendence/Backend/start.sh
 
 # Run the gunicorn command
 gunicorn hello_django.wsgi:application --bind 0.0.0.0:8000
