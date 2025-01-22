a9a270e8e979cc1dea7687e6ff8953f1cefc6c68
Jorge Costa <Kelho88@pm.me>
2025-01-22 17:20:07 +0000

    feat: integrate JWT authentication and update throttling settings for user endpoints

--- Backend/users/views.py
+++ Backend/users/views.py
@@ -1,20 +1,23 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view, throttle_classes
+from rest_framework.decorators import api_view, throttle_classes, permission_classes
 from rest_framework import status
 from .serializers import SiteUserSerializer
 from .models import SiteUser
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 from rest_framework.throttling import AnonRateThrottle
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 class RegisterUserThrottle(AnonRateThrottle):
     rate = '4/hour'  # Custom throttle rate for user registration
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated, IsAdminUser])
 def getUsersData(request):
     """
     Retrieve a list of all users.
     """
+    
     SiteUsers = SiteUser.objects.all()
     serializer = SiteUserSerializer(SiteUsers, many=True)
     return Response(serializer.data)
@@ -28,6 +31,7 @@ def getUsersData(request):
     }
 )
 @api_view(['POST'])
+@permission_classes([])
 @throttle_classes([RegisterUserThrottle])
 def create_user(request):
     """
