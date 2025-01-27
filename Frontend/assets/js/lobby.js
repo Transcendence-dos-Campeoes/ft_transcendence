ed9bdad7ce140a4ddea04cc2421ec23d65bee57b
masoares <manueljorgecruz@hotmail.com>
2025-01-27 13:38:34 +0000

    all active people appear on friends list

--- Frontend/assets/js/lobby.js
+++ Frontend/assets/js/lobby.js
@@ -2,10 +2,13 @@
 
 function fetchAvailablePlayers() {
     const accessToken = localStorage.getItem('token');
+    const currentUser = sessionStorage.getItem('username');
+
     if (!accessToken) {
         console.error('Access token not found');
         return;
     }
+
     fetch('http://localhost:8000/api/users/available-players/', {
         method: 'GET',
         headers: {
@@ -16,13 +19,22 @@ function fetchAvailablePlayers() {
     .then(response => response.json())
     .then(data => {
         console.log('API response:', data); // Debugging line
+        const playersList = document.getElementById('online-players-list');
+        //playersList.innerHTML = '';
         if (Array.isArray(data)) {
-            const playersList = document.getElementById('players-list');
-            playersList.innerHTML = '';
             data.forEach(player => {
-                const li = document.createElement('li');
-                li.textContent = player.username;
-                playersList.appendChild(li);
+                if (player.username !== currentUser) {
+                    const a = document.createElement('a');
+                    a.href = '#';
+                    a.className = 'list-group-item list-group-item-action py-3 lh-sm';
+                    a.innerHTML = `
+                        <div class="d-flex w-100 align-items-center justify-content-between">
+                            <strong class="mb-1">${player.username}</strong>
+                            <small class="text-body-secondary">Online</small>
+                        </div>
+                    `;
+                    playersList.appendChild(a);
+                }
             });
         } else {
             console.error('Expected an array but got:', data);
@@ -33,5 +45,5 @@ function fetchAvailablePlayers() {
 
 document.addEventListener('DOMContentLoaded', function() {
     fetchAvailablePlayers();
-    setInterval(fetchAvailablePlayers, 5000); // Refresh every 5 seconds
+    //setInterval(fetchAvailablePlayers, 5000); // Refresh every 5 seconds
 });
