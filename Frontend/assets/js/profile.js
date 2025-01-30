e790c59085a9f16ea3456f66c7f72f221e03a5e2
Rui Pires <ruipedro.pires999@gmail.com>
2025-01-30 10:21:32 +0000

    feat: add user settings page and functionality to retrieve user settings

--- Frontend/assets/js/profile.js
+++ Frontend/assets/js/profile.js
@@ -1,62 +1,3 @@
-async function attachProfileFormListener() {
-  const form = document.getElementById("profile-form");
-  if (!form) {
-    console.error("Profile form not found");
-    return;
-  }
-
-  form.addEventListener("submit", async (event) => {
-    event.preventDefault();
-    console.log("📝 Submitting profile form");
-    const loadingOverlay = new LoadingOverlay();
-
-    const username = document.getElementById("username-input").value;
-    const email = document.getElementById("email-input").value;
-    const twoFactorEnabled = document.getElementById("2fa-toggle").checked;
-
-    try {
-      loadingOverlay.show();
-      const response = await fetch(
-        "http://localhost:8000/api/users/profile/update/",
-        {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${localStorage.getItem("access")}`,
-          },
-          body: JSON.stringify({
-            username: username,
-            email: email,
-            two_factor_enabled: twoFactorEnabled,
-          }),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Failed to update profile");
-      }
-
-      const data = await response.json();
-      console.log("✅ Profile updated:", data);
-
-      // Update form with new data
-      document.getElementById("username-input").value = data.username;
-      localStorage.setItem("username", data.username);
-      document.getElementById("email-input").value = data.email;
-      document.getElementById("2fa-toggle").checked = data.two_factor_enabled;
-
-      // Show success message
-      displayMessage("Profile updated successfully", MessageType.SUCCESS);
-      renderElement("overview");
-    } catch (error) {
-      console.error("❌ Error updating profile:", error);
-      displayMessage("Failed to update profile", MessageType.ERROR);
-    } finally {
-      loadingOverlay.hide();
-    }
-  });
-}
-
 async function loadProfileData() {
   try {
     const response = await fetch("http://localhost:8000/api/users/profile/", {
@@ -70,12 +11,6 @@ async function loadProfileData() {
     }
 
     const data = await response.json();
-
-    // Update profile form and details
-    document.getElementById("username-input").value = data.username;
-    document.getElementById("email-input").value = data.email;
-    document.getElementById("profile-username").textContent = data.username;
-    document.getElementById("2fa-toggle").checked = data.two_fa_enabled;
 
     // Format and display creation date
     const createdDate = new Date(data.created_time).toLocaleDateString(
