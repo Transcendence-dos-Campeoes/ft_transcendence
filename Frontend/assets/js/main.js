284af7456c3703bb9ad56d2fdb2565113000449c
Rui Pires <ruipedro.pires999@gmail.com>
2025-02-12 19:22:41 +0000

    Remove redundant page check in renderPage function for cleaner code

--- Frontend/assets/js/main.js
+++ Frontend/assets/js/main.js
@@ -51,7 +51,6 @@ async function isAuthenticated() {
 async function renderPage(page) {
 	console.log(`Attempting to render page: ${page}`);
 	const loadingOverlay = new LoadingOverlay();
-	if (router.currentPage === page) return;
 
 	if (page === "home" || page === "pong") {
 		const authenticated = await isAuthenticated();
